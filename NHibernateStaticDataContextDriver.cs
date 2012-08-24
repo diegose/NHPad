a616e1d6faa3acf0e187dfcf3b718b74aacac7ce
Diego Mijelshon <diego@mijelshon.com.ar>
2012-08-24 17:46:57 -0300

    Initial boilerplate

---
+++ NHibernateStaticDataContextDriver.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using LINQPad.Extensibility.DataContext;
+
+namespace NHPad
+{
+    public class NHibernateStaticDataContextDriver : StaticDataContextDriver
+    {
+        public override string GetConnectionDescription(IConnectionInfo cxInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool ShowConnectionDialog(IConnectionInfo cxInfo, bool isNewConnection)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Name
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override string Author
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override List<ExplorerItem> GetSchema(IConnectionInfo cxInfo, Type customType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

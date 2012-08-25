6efa2e4cbf6b6b611e063147240c9172c723e07e
Diego Mijelshon <diego@mijelshon.com.ar>
2012-08-24 22:01:32 -0300

    Added connection dialog

---
+++ ConnectionDialog.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Xml.Linq;
+using LINQPad.Extensibility.DataContext;
+
+namespace NHPad
+{
+    public partial class ConnectionDialog
+    {
+        public ConnectionDialog(IConnectionInfo cxInfo)
+        {
+            InitializeComponent();
+            DataContext = new DriverDataWrapper(cxInfo.DriverData);
+        }
+
+        private void HandleOk(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+    }
+
+    public class DriverDataWrapper
+    {
+        readonly XElement driverData;
+
+        public DriverDataWrapper(XElement driverData)
+        {
+            this.driverData = driverData;
+        }
+
+        public string ClientAssembly
+        {
+            get { return (string)driverData.Element("ClientAssembly"); }
+            set { driverData.SetElementValue("ClientAssembly", value); }
+        }
+    }
+}
